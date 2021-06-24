85778f76762bc974144fb29ddd2e77987120d895
Jason <chai061094@gmail.com>
2021-06-24 17:13:13 +0800

    add local settings to server

--- hehe/settings.py
+++ hehe/settings.py
@@ -138,3 +138,8 @@ MEDIA_URL='/media1/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
